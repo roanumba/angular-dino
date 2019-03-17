96ae1affc4ddf2e7e8ea6419d5885690f2008768
raphael <roanumba@gmail.com>
2019-03-16 17:52:57 -0700

    commit 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DataAndMethodComponent} from "./data-and-method/data-and-method.component";
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+              path: '',
+              component: HomeComponent,
+          },
+          {
+              path: 'about',
+              component: AboutComponent,
+          },
+          {
+              path: 'dataNmethod',
+              component: DataAndMethodComponent,
+          },
+          // {
+          //     path: '/computeNwatch',
+          //     component: ComputeAndWatch,
+          // },
+          // {
+          //     path: '/classStyleBind',
+          //     component: ClassStyleBind,
+          // },
+          // {
+          //     path: '/filter',
+          //     component: Filters,
+          // },
+          // {
+          //     path: '/vueComponent',
+          //     component: VueComponent,
+          // },
+          // {
+          //     path: '/vuetifyMaterial',
+          //     component: VuetifyMaterial,
+          // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataAndMethodComponent } from './data-and-method/data-and-method.component';
+import {FormsModule} from "@angular/forms";
+import { HomeComponent } from './home/home.component';
+import { HelloComponent } from './hello/hello.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataAndMethodComponent,
+    HomeComponent,
+    HelloComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule ,
   ],
   providers: [],
   bootstrap: [AppComponent]
